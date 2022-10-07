3d16b402a29bc6efbe994a9b2b5f769f6d250081
NicoArguello1979 <nicolasarguello1979@gmail.com>
2022-10-07 15:47:21 -0300

    Actualizando formularios 3

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -7,11 +7,12 @@ const usuarioController = require('../controllers/usuarioController');
 const uploadFile = require ('../middlewares/multerMiddleware');
 const validations = require ('../middlewares/validateRegisterMiddleware');
 
-router.get('/', usuarioController.login); 
-
-router.post('/register', usuarioController.Register);
+router.get('/', usuarioController.register); 
+router.get('/register', usuarioController.register); 
+router.post('/register', usuarioController.register);
 router.post('/register', uploadFile.single('avatar'), validations, usuarioController.processRegister);
 
+router.get('/', usuarioController.login); 
 router.get('/login', usuarioController.login); 
 router.post('/login', usuarioController.loginProcess);
 
